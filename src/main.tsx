21ca899f027c9b36a5f3f72eaffeceafb57bd826
Ahmed Rabbi <99005852+itsahrabbi@users.noreply.github.com>
2024-07-08 20:44:48 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GitProfile from './components/gitprofile.tsx';
 
+export const UNSIGNED = 1.8
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GitProfile config={CONFIG} />
